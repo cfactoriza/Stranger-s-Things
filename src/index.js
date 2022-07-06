e2f557b70e2cbf251ad64024b32d53065e6922bc
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-06 10:23:42 -0500

    fixed rendering bug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { App } from "./components";
 
 //TESTING PUSH REQUESTS
 
 
-const root = ReactDOM.createRoot(document.getElementByID('app'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <BrowserRouter>
     <App />
