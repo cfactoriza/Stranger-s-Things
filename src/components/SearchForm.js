14f63de1fd7db3355c5cfb780557d053c7e4d315
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-09 18:29:13 -0500

    modified the search box

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,42 +1,89 @@
 import React, { useState } from "react";
-import Post from './Post'
+import Post from "./Post";
 
 const SearchForm = (props) => {
   const [searchPost, setSearchPost] = useState([]);
-  const { posts, setShowMyPosts, token, setPostsList, setSearchingPost} = props;
+  const { posts, setShowMyPosts, token, setPostsList, setSearchingPost } =
+    props;
   function handleChange(event) {
     const searchTerm = event.target.value;
-    console.log(searchTerm)
+    console.log(searchTerm);
     function postMatches(post, text) {
       if (post.title.includes(text)) {
-        setSearchingPost(true)
+        setSearchingPost(true);
         return true;
       } else if (post.description.includes(text)) {
-        setSearchingPost(true)
+        setSearchingPost(true);
         return true;
       } else {
         return false;
       }
-
     }
     const filteredPosts = posts.filter((post) => postMatches(post, searchTerm));
     const postsToDisplay = searchTerm.length ? filteredPosts : posts;
     setSearchPost(postsToDisplay);
     if (postsToDisplay.length === 0) {
-      setSearchingPost(false) 
+      setSearchingPost(false);
     }
-    
   }
 
   return (
     <div>
-      <div>
-        <label>Search Form</label>
-        <input onChange={handleChange} placeholder="Enter keywords..."></input>
+
+      {/* <div className="form-outline">
+        <input
+          type="search"
+          id="form1"
+          className="form-control"
+          placeholder="Type query"
+          aria-label="Search"
+          onChange={handleChange}
+        />
+        
+      </div> */}
+      <section className="intro">
+  <div className="bg-image h-100" style={{
+          backgroundImage: "url(https://images.creativemarket.com/0.1.0/ps/5319914/3008/1003/m1/fpnw/wm0/_dsc60102-.jpg?1541415014&s=7c8a8e0bad89065f858412e2bc2e9c06)"}}>
+    <div className="mask d-flex align-items-center h-100" style={{backgroundColor: "rgba(0,0,0, 0.5)"}}>
+      <div className="container">
+        <p className="h2 mb-4 text-white" style={{paddingTop: "1rem", textAlign:"center"}}>Stranger's Things, the best website to shop for items </p>
+        <div className="card">
+          <div className="card-body p-4">
+            <div className="row">
+              <div className="col-12 mb-6">
+                <div className="input-group">
+                  <div className="form-outline flex-fill">
+                    <input type="search" id="searchPosts" className="form-control form-control-lg" onChange={handleChange}/>
+                    <label className="form-label" htmlFor="searchPosts">Search for Posts</label>
+                    <div className="form-helper pt-2"><span className="font-weight-bold">Suggested: </span> 
+                      <a href="#!" className="text-black-50">car, </a>  
+                      <a href="#!" className="text-black-50">puppy, </a>  
+                      <a href="#!" className="text-black-50">bike, </a>  
+                      <a href="#!" className="text-black-50">phone, </a>  
+                      <a href="#!" className="text-black-50">house, </a>  
+                      <a href="#!" className="text-black-50">chair, </a>  
+                      <a href="#!" className="text-black-50">and more</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-        {
-          searchPost.length > 0 ? <Post posts={searchPost} setShowMyPosts={setShowMyPosts} token={token} setPostsList={setPostsList}/> : null
-        }
+    </div>
+  </div>
+</section>
+
+      {searchPost.length > 0 ? (
+        <Post
+          posts={searchPost}
+          setShowMyPosts={setShowMyPosts}
+          token={token}
+          setPostsList={setPostsList}
+        />
+      ) : null}
+
     </div>
   );
 };
